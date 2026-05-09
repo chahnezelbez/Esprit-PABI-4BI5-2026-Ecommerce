eb631a55fc653d04c47e844daa68eed8b8c72b59
sabrine0602 <sabrinezaddem18@gmail.com>
2026-05-09 17:23:17 +0100

    keycloack

--- frontend/src/app/keycloak/keycloak-init.factory.ts
+++ frontend/src/app/keycloak/keycloak-init.factory.ts
@@ -4,7 +4,7 @@ export function initializeKeycloak(keycloak: KeycloakService) {
   return () =>
     keycloak.init({
    config: {
-  url: 'http://localhost:8180',
+  url: 'https://sougui-keycloakv1.onrender.com',
   realm: 'sougui-realm',
   clientId: 'angular-app'
 },
